3ab9ec040581c4073afa55132abb13d06ea54cdf
Marko Marinović <marko.marinovic18@student.fsre.ba>
2023-05-24 10:55:25 +0200

    text fix

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 const Navbar = () => {
     return ( 
         <nav className="navbar">
-            <h1>Pozdrav brate</h1>
+            <h1>Blog List</h1>
             <div className="links"></div>
             <Link to="/">Home</Link>
             <Link to="/create">New Blog</Link>
